d8a3032a4f0784bfcc1a42a95d378526e3837ddf
Damien John Melksham <Damien.Melksham@gmail.com>
2016-04-21 23:24:44 +1000

    Automatic peasant commit message!

--- gittest.lisp
+++ gittest.lisp
@@ -3,9 +3,7 @@
 (defvar *project-path* nil)
 
 (defun set-gittest-active-directory (path/string)
-  (if (pathnamep path/string)
-      (setf *project-path* path/string)
-      (setf *project-path* (uiop:parse-unix-namestring path/string))))
+  (setf *project-path* (uiop:ensure-directory-pathname path/string)))
 
 (defun make-git-command (command)
   (concatenate 'string "cd " (namestring *project-path*) " && git " command))
@@ -15,16 +13,15 @@
 			:on-error nil)))
 
 (defun add-emacs-git-ignore ()
-  (if (or (null *project-path*)
-	  (not (probe-file *project-path*)))
-      (return-from add-emacs-git-ignore)
-      (with-open-file (stream (pathname
-			       (concatenate 'string (namestring *project-path*) ".gitignore"))
+  (if (null *project-path*)
+      (return-from add-emacs-git-ignore nil)
+      (with-open-file (stream (uiop:merge-pathnames* *project-path* ".gitignore")
 			      :direction :output
 			      :if-exists :supersede
 			      :if-does-not-exist :create)
 
 	(format stream "~a~&" "#Ignore emacs editor temporary files")
 	(format stream "~a~&" "[#]*[#]")
-	(format stream "~a~&" "*~"))))
+	(format stream "~a~&" "*~")))
+  t)
     
